7bea20fbe0a2d79eb4e954fe55987a9e05f8c8ba
Arttu Aarnio <arttueino@gmail.com>
2024-09-16 17:07:09 +0300

    added the functionality to edit a book

--- src/main/java/hh/sof3/bookstore/web/BookController.java
+++ src/main/java/hh/sof3/bookstore/web/BookController.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
 import hh.sof3.bookstore.domain.Book;
 import hh.sof3.bookstore.domain.BookRepository;
 
@@ -42,8 +41,22 @@ public class BookController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteBook(@PathVariable("id") Long bookId, Model model){
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         repository.deleteById(bookId);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") long bookId, Model model) {
+        Book book = repository.findById(bookId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid book Id:" + bookId));;
+        model.addAttribute("book", book);
+        return "editbook";
+    }
+
+    @PostMapping("/update")
+    public String updateBook(@ModelAttribute Book book){
+        repository.save(book);
         return "redirect:/books";
     }
 }
