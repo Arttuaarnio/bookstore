3b9ab6954f912ee5bdc5b25f02f7f939b8b49a3e
Arttu Aarnio <arttueino@gmail.com>
2024-09-16 15:23:21 +0300

    added a listpage for the books

--- src/main/java/hh/sof3/bookstore/web/BookController.java
+++ src/main/java/hh/sof3/bookstore/web/BookController.java
@@ -1,14 +1,27 @@
 package hh.sof3.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.sof3.bookstore.domain.BookRepository;
 
 @Controller
 
 public class BookController {
 
+    @Autowired
+    private BookRepository repository;
+
     @GetMapping("/index")
     public String bookstoreHomepage() {
         return "welcomepage";
+    }
+
+    @GetMapping("/books")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
